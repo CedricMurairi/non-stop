a2dda73d2931ac3abb7c2e7463961766d426f604
Cedric Murairi <murairicedric@gmail.com>
2021-06-04 15:18:20 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Todo extends React.Component{
+  constructor(props){
+    super(props);
+    this.createTask = this.createTask.bind(this)
+    this.state = {
+      tasks: JSON.parse(localStorage.getItem('tasks')),
+      labels: JSON.parse(localStorage.getItem('labels'))
+    }
+  }
+
+  createTask(e){
+    e.preventDefault();
+    const data = new FormData(e.target);
+    this.setState({tasks: [...this.state.tasks, {
+      title: data.get('title'),
+      description: data.get('description'),
+      done: false
+    }]})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ToDO App | Hack your Day</h1>
+        <hr/>
+        <div>
+          <form className="mb-4" onSubmit={this.createTask}>
+            <input
+              required
+              className="form-control mb-2"
+              type="text"
+              placeholder="Title"
+              name="title"
+            />
+            <textarea required placeholder="Description here!" className="form-control mb-2" name="description"></textarea>
+            <button className="btn btn-primary">Create Task</button>
+          </form>
+        </div>
+        <h3>Tasks</h3>
+        <hr/>
+        <div className="tasks-list">
+          {this.state.tasks.map((task, index) => {
+            return(
+              <div key={index} className="taks">
+                  <input className="form-check-input" type="checkbox" value=""></input>
+                  <h4 className={task.done ? "line-through" : null}>{task.title}</h4>
+                  <p>{task.description}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Todo />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
