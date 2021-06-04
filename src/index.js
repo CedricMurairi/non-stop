49819e07267e1e48f7dfef260b875ae6c05c61ac
Cedric Murairi <murairicedric@gmail.com>
2021-06-04 17:03:28 +0200

    user can complete task now and it gets a line through and checkbox on :hammer:

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ class Todo extends React.Component{
   constructor(props){
     super(props);
     this.createTask = this.createTask.bind(this)
+    this.complete = this.complete.bind(this)
     this.state = {
       tasks: JSON.parse(localStorage.getItem('tasks')) || [],
       labels: JSON.parse(localStorage.getItem('labels')) || []
@@ -28,6 +29,14 @@ class Todo extends React.Component{
     this.setState({tasks: JSON.parse(localStorage.getItem('tasks'))})
   }
 
+  complete(e){
+    const taskIndex = e.target.dataset.key
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    tasks[taskIndex].done = true
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    this.setState({tasks: JSON.parse(localStorage.getItem('tasks'))})
+  }
+
   render() {
     return (
       <div>
@@ -43,7 +52,7 @@ class Todo extends React.Component{
               name="title"
             />
             <textarea required placeholder="Description here!" className="form-control mb-2" name="description"></textarea>
-            <button className="btn btn-primary">Create Task</button>
+            <button className="btn btn-secondary">Create Task</button>
           </form>
         </div>
         <h3>Tasks</h3>
@@ -51,10 +60,12 @@ class Todo extends React.Component{
         <div className="tasks-list">
           {this.state.tasks.map((task, index) => {
             return(
-              <div key={index} className="taks">
-                  <input className="form-check-input" type="checkbox" value=""></input>
-                  <h4 className={task.done ? "line-through" : null}>{task.title}</h4>
-                  <p>{task.description}</p>
+              <div key={index} className="tasks">
+                  <div className="d-flex">
+                    <input checked={task.done ? true : false} data-key={index} className="form-check-input" type="checkbox" onChange={this.complete}></input>
+                    <h4 className={task.done ? "line-through" : null}>{task.title}</h4>
+                  </div>
+                  <p className="description">{task.description}</p>
               </div>
             )
           })}
