af20fb7e3445d541216ca2bf8ad354cf2f10ca95
Cedric Murairi <murairicedric@gmail.com>
2021-06-05 14:35:09 +0200

    changed the design from a flex to grid + flex layout

--- src/index.js
+++ src/index.js
@@ -19,14 +19,27 @@ class Todo extends React.Component{
     e.preventDefault();
     const data = new FormData(e.target);
     let tasks = JSON.parse(localStorage.getItem('tasks'))
-    tasks = [...tasks, {
-      title: data.get('title'),
-      description: data.get('description'),
-      done: false
-    }]
+    if (tasks){
+      tasks = [...tasks, {
+        title: data.get('title'),
+        description: data.get('description'),
+        done: false
+      }]
+    }else{
+      tasks = [
+        {
+          title: data.get('title'),
+          description: data.get('description'),
+          done: false
+        }
+      ]
+    }
 
     localStorage.setItem('tasks', JSON.stringify(tasks))
     this.setState({tasks: JSON.parse(localStorage.getItem('tasks'))})
+
+    e.target.title = ""
+    e.target.description = ""
   }
 
   complete(e){
@@ -41,70 +54,76 @@ class Todo extends React.Component{
   render() {
     return (
       <div>
-        <h1>ToDO App | Hack your Day</h1>
-        <hr/>
-        <div className="side-bar left-bar">
-          <form className="label-form mb-4">
-            <input
-              required
-              type="text"
-              className="form-control mb-2"
-              placeholder="Label name"
-              name="label-name"
-            />
-            <input
-              required
-              type="color"
-              className="form-control mb-2"
-              name="label-color"
-            />
-            <button className="btn btn-secondary">Create Label</button>
-          </form>
-          <h3>Labels</h3>
+        <div className="mgin-40">
+          <h1>HackDay</h1>
           <hr/>
         </div>
-        <div className="side-bar right-bar">
-          <form className="project-form mb-4">
-            <input
-              required
-              type="text"
-              className="form-control mb-2"
-              placeholder="Project name"
-              name="project-name"
-            />
-            <textarea required placeholder="Description here!" className="form-control mb-2" name="project-description"></textarea>
-            <button className="btn btn-secondary">Create Project</button>
-          </form>
-          <h3>Projects</h3>
-          <hr/>
+        <div className="span-across">
+          <div className="side-bar left-bar">
+            <form className="label-form mb-4">
+              <input
+                required
+                type="text"
+                className="form-control mb-2"
+                placeholder="Label name"
+                name="label-name"
+              />
+              <input
+                required
+                type="color"
+                className="form-control mb-2"
+                name="label-color"
+              />
+              <button className="btn btn-secondary">Create Label</button>
+            </form>
+            {/* <h3>Labels</h3> */}
+            {/* <hr/> */}
+          </div>
+          <div className="main-form">
+            <form className="mb-4" onSubmit={this.createTask}>
+              <input
+                required
+                className="form-control mb-2"
+                type="text"
+                placeholder="Title"
+                name="title"
+              />
+              <textarea required placeholder="Description here!" className="form-control mb-2" name="description"></textarea>
+              <button className="btn btn-secondary">Create Task</button>
+            </form>
+          </div>
+          <div className="side-bar right-bar">
+            <form className="project-form mb-4">
+              <input
+                required
+                type="text"
+                className="form-control mb-2"
+                placeholder="Project name"
+                name="project-name"
+              />
+              <textarea required placeholder="Description here!" className="form-control mb-2" name="project-description"></textarea>
+              <button className="btn btn-secondary">Create Project</button>
+            </form>
+            {/* <h3>Projects</h3> */}
+            {/* <hr/> */}
+          </div>
         </div>
-        <div>
-          <form className="mb-4" onSubmit={this.createTask}>
-            <input
-              required
-              className="form-control mb-2"
-              type="text"
-              placeholder="Title"
-              name="title"
-            />
-            <textarea required placeholder="Description here!" className="form-control mb-2" name="description"></textarea>
-            <button className="btn btn-secondary">Create Task</button>
-          </form>
-        </div>
-        <h3>Tasks</h3>
-        <hr/>
-        <div className="tasks-list">
-          {this.state.tasks.map((task, index) => {
-            return(
-              <div key={index} className={task.done ? "done tasks" : "tasks"}>
-                  <div className="d-flex">
-                    <input checked={task.done ? true : false} data-key={index} className="form-check-input" type="checkbox" onChange={this.complete}></input>
-                    <h5 className={task.done ? "line-through" : null}>{task.title}</h5>
-                  </div>
-                  <p className="description">{task.description}</p>
-              </div>
-            )
-          })}
+        <div className="mgin-40">
+          {/* <h3>Tasks</h3> */}
+          {/* <hr/> */}
+          <div className="tasks-list">
+            {this.state.tasks.map((task, index) => {
+              return(
+                <div key={index} className={task.done ? "done tasks" : "tasks"}>
+                    <div className="d-flex">
+                      <input checked={task.done ? true : false} data-key={index} className="form-check-input" type="checkbox" onChange={this.complete}></input>
+                      <h5 className={task.done ? "line-through" : null}>{task.title}</h5>
+                    </div>
+                    <p className="description">{task.description}</p>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     )
