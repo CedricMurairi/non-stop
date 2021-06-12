c3d9c3fd38d98517a49c43a9dfb1a99b239ecfc1
Cedric Murairi <murairicedric@gmail.com>
2021-06-12 18:32:22 +0200

    added event handlers for click on edit and delete buttons

--- src/custom.js
+++ src/custom.js
@@ -1,8 +1,11 @@
 
 window.onload = () => {
+    let editForm = document.querySelectorAll(".create-task")
+
     let tasks = document.querySelectorAll(".task")
     tasks.forEach(task => task.childNodes[0].childNodes[1].addEventListener('click', () => {
-        task.childNodes[2].classList.toggle('hidden');
+        task.childNodes[3].classList.toggle('hidden');
+        task.childNodes[2].classList.toggle('hidden')
         task.childNodes[1].classList.toggle('hidden')
     }))
 
@@ -10,4 +13,23 @@ window.onload = () => {
     projects.forEach(project => project.addEventListener('click', () => {
         project.childNodes[1].classList.toggle('hidden');
     }))
+
+    let taskOptions = document.querySelectorAll(".editTask")
+    taskOptions.forEach(option => option.addEventListener('click', (e) => {
+        let target = e.target.parentElement
+        editForm[0].childNodes[2].classList.add("hidden")
+        editForm[0].childNodes[3].classList.remove("hidden")
+        editForm[0].childNodes[4].classList.remove("hidden")
+        editForm[0].dataset.id = e.target.dataset.id
+        editForm[0].childNodes[0].value = target.childNodes[0].childNodes[1].innerText
+        editForm[0].childNodes[1].value = target.childNodes[3].innerText
+    }))
+
+    let cancel = document.querySelector(".cancel")
+    cancel.addEventListener('click', (e) => {
+        e.target.parentElement.reset()
+        editForm[0].childNodes[2].classList.remove("hidden")
+        editForm[0].childNodes[3].classList.add("hidden")
+        editForm[0].childNodes[4].classList.add("hidden")
+    })
 }
