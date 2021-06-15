1a2453421d0eceb97adaac092b9709a529112bef
Cedric Murairi <murairicedric@gmail.com>
2021-06-15 04:28:21 +0200

    fixed issue with event listners on event edit

--- src/custom.js
+++ src/custom.js
@@ -3,7 +3,7 @@ window.onload = () => {
     let editForm = document.querySelectorAll(".create-task")
 
     let tasks = document.querySelectorAll(".task")
-    tasks.forEach(task => task.childNodes[0].childNodes[1].addEventListener('click', () => {
+    tasks.forEach(task => task.childNodes[0].childNodes[1].childNodes[0].addEventListener('click', () => {
         task.childNodes[3].classList.toggle('hidden');
         task.childNodes[2].classList.toggle('hidden')
         task.childNodes[1].classList.toggle('hidden')
@@ -17,27 +17,30 @@ window.onload = () => {
     let taskOptions = document.querySelectorAll(".editTask")
     taskOptions.forEach(option => option.addEventListener('click', (e) => {
         let target = e.target.parentElement
-        editForm[0].childNodes[2].classList.add("hidden")
-        editForm[0].childNodes[3].classList.remove("hidden")
+        editForm[0].childNodes[3].classList.add("hidden")
         editForm[0].childNodes[4].classList.remove("hidden")
+        editForm[0].childNodes[5].classList.remove("hidden")
+        editForm[0].childNodes[2].classList.remove("hidden")
         editForm[0].dataset.id = e.target.dataset.id
-        editForm[0].childNodes[0].value = target.childNodes[0].childNodes[1].innerText
+        editForm[0].childNodes[0].value = target.childNodes[0].childNodes[1].childNodes[0].innerText
         editForm[0].childNodes[1].value = target.childNodes[3].innerText
     }))
 
     let edit = document.querySelector(".edit")
     edit.addEventListener('click', (e) => {
         // e.target.parentElement.reset()
-        editForm[0].childNodes[2].classList.remove("hidden")
-        editForm[0].childNodes[3].classList.add("hidden")
+        editForm[0].childNodes[3].classList.remove("hidden")
         editForm[0].childNodes[4].classList.add("hidden")
+        editForm[0].childNodes[5].classList.add("hidden")
+        editForm[0].childNodes[2].classList.add("hidden")
     })
 
     let cancel = document.querySelector(".cancel")
     cancel.addEventListener('click', (e) => {
         e.target.parentElement.reset()
-        editForm[0].childNodes[2].classList.remove("hidden")
-        editForm[0].childNodes[3].classList.add("hidden")
+        editForm[0].childNodes[3].classList.remove("hidden")
         editForm[0].childNodes[4].classList.add("hidden")
+        editForm[0].childNodes[5].classList.add("hidden")
+        editForm[0].childNodes[2].classList.add("hidden")
     })
 }
